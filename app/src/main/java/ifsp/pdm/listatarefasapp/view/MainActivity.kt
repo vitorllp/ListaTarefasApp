80fe3d482f6a89c75a12aae7d6f9f2a5f7e1ab53
vitorllp <vitorlpastore11@gmail.com>
2021-07-12 23:00:00 -0300

    implementando logout

--- app/src/main/java/ifsp/pdm/listatarefasapp/view/MainActivity.kt
+++ app/src/main/java/ifsp/pdm/listatarefasapp/view/MainActivity.kt
@@ -1,13 +1,42 @@
 package ifsp.pdm.listatarefasapp.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import ifsp.pdm.listatarefasapp.R
+import ifsp.pdm.listatarefasapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = Firebase.auth.currentUser
+        val textoUsuario: String = "Bem-vindo," + user?.email
+        activityMainBinding.textUsuario.text = textoUsuario
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.sairMI){
+            Firebase.auth.signOut()
+            startActivity(Intent(this, AutenticacaoActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
